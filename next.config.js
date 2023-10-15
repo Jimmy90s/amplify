950221d18efdaa4927aee6a3d039b097992fe348
jimmy90s <jimmy90s@outlook.com>
2023-10-15 08:42:06 -0700

    next config edit

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-module.exports = module.exports = {
-  nextConfig,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "fullstackdotso.nyc3.cdn.digitaloceanspaces.com",
-      },
-    ],
-  },
+module.exports = {
+  reactStrictMode: false,
 };
